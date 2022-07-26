be8ade32224195a9043eb2ba67375b268abd078e
WpDavi <100159858+WpDavi@users.noreply.github.com>
2022-07-26 17:53:42 -0300

    Initial commit

---
+++ npms.js
@@ -0,0 +1,14 @@
+/*
+npm install @react-navigation/native
+npm install react-native-screens react-native-safe-area-context
+npm install @react-navigation/native-stack
+npm install @react-navigation/bottom-tabs
+npm install @react-native-async-storage/async-storage
+npm install @react-native-community/geolocation --save
+npm install --save react-native-gesture-handler
+npm install --save react-native-permissions  // tem que add permission
+npm install react-native-svg
+npm install react-native-svg-transformer
+yarn add styled-components
+
+*/
