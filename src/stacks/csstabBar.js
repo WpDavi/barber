9c990ecd46645caff604a52ba3ea62fb8d049f34
WpDavi <100159858+WpDavi@users.noreply.github.com>
2022-07-28 10:44:40 -0300

    Update csstabBar.js

--- src/stacks/csstabBar.js
+++ src/stacks/csstabBar.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { StyleSheet, TouchableOpacity, View } from 'react-native'
 
+
+
 import HomeIcon from '../assets/home.svg'
 import SearchIcon from '../assets/search.svg'
 import TodayIcon from '../assets/today.svg'
 import FavoriteIcon from '../assets/favorite.svg'
 import AccountIcon from '../assets/account.svg'
 
-export default function tabBar() {
+export default function tabBar({ state, navigation }) {
+    
+    const goTo = (screenName) => {
+        navigation.navigation(screenName);
+    }
+
     return (
         <View style={styles.containertab}>
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+            onPress={()=>goTo(Home) } >                
                 <HomeIcon widht='24' height='24' fill='white' />
             </TouchableOpacity>
             
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button}
+            onPress={()=>goTo(Search) } >
                 <SearchIcon widht='24' height='24' fill='white' />
             </TouchableOpacity>
 
